cc39f8d9fd9b737d1c296e3d4fc1317cc9b9bd52
sergey.tkachenko <serg.tkachenko@hotmail.com>
2019-09-26 00:46:17 +0300

    add pipelines page

--- src/views/Pipelines.vue
+++ src/views/Pipelines.vue
@@ -5,100 +5,115 @@
 			<v-container class="fill-height" fluid>
 				<v-layout>
 					<v-flex>
-						<h1>Pipelines</h1>
-						<v-simple-table>
-							<template v-slot:default>
-								<thead>
-									<tr>
-										<th class="text-left">Run status</th>
-										<th class="text-left">Name</th>
-										<th class="text-left">launches</th>
-										<th class="text-left">Last completed</th>
-										<th class="text-left">Scrap data</th>
-										<th class="text-left">Actions</th>
-									</tr>
-								</thead>
-								<tbody>
-									<tr v-for="pipeline in pipelines" :key="pipeline.id">
-										<td>
-											<pipeline-run-status-button
-												:pipeline="pipeline"
-											></pipeline-run-status-button>
-										</td>
-										<td>{{ pipeline.key }}</td>
-										<td>
-											<v-btn
-												text
-												small
-												color="primary"
-												:to="'/tasks/' + pipeline.id"
-												>35 launches</v-btn
-											>
-										</td>
-										<td>{{ fromNow(pipeline.lastCompletedOn) }}</td>
-										<td>
-											<v-menu offset-y>
-												<template v-slot:activator="{ on }">
-													<v-btn color="primary" text small v-on="on">
-														download
-													</v-btn>
-												</template>
-												<v-list>
-													<v-list-item>
-														<a
-															:href="
-																'/api/pipeline/data/' + pipeline.id
-															"
-															target="_blank"
-														>
-															json
-														</a>
-													</v-list-item>
-													<v-list-item>
-														<a
-															:href="
-																'/api/pipeline/data/csv/' +
-																	pipeline.id
-															"
-															target="_blank"
-														>
-															csv
-														</a>
-													</v-list-item>
-												</v-list>
-											</v-menu>
-										</td>
-										<td>
-											<v-menu offset-y>
-												<template v-slot:activator="{ on }">
-													<v-btn depressed text v-on="on">
-														<v-icon>more_vert</v-icon>
-													</v-btn>
-												</template>
-												<v-list>
-													<v-list-item @click="edit(pipeline.id)">
-														<v-list-item-title>
-															<v-icon style="padding-right: 5px;"
-																>create</v-icon
+						<v-card>
+							<v-card-title>
+								<span>Pipelines</span>
+								<v-btn
+									color="accent"
+									small
+									depressed
+									style="margin-left: 20px;"
+									to="/pipeline/add"
+								>
+									<v-icon>add</v-icon>
+									add new pipeline
+								</v-btn>
+							</v-card-title>
+							<v-simple-table>
+								<template v-slot:default>
+									<thead>
+										<tr>
+											<th class="text-left">Run status</th>
+											<th class="text-left">Name</th>
+											<th class="text-left">launches</th>
+											<th class="text-left">Last completed</th>
+											<th class="text-left">Scrap data</th>
+											<th class="text-left">Actions</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr v-for="pipeline in pipelines" :key="pipeline.id">
+											<td>
+												<pipeline-run-status-button
+													:pipeline="pipeline"
+												></pipeline-run-status-button>
+											</td>
+											<td>{{ pipeline.key }}</td>
+											<td>
+												<v-btn
+													text
+													small
+													color="primary"
+													:to="'/tasks/' + pipeline.id"
+													>35 launches</v-btn
+												>
+											</td>
+											<td>{{ fromNow(pipeline.lastCompletedOn) }}</td>
+											<td>
+												<v-menu offset-y>
+													<template v-slot:activator="{ on }">
+														<v-btn color="primary" text small v-on="on">
+															download
+														</v-btn>
+													</template>
+													<v-list>
+														<v-list-item>
+															<a
+																:href="
+																	'/api/pipeline/data/' +
+																		pipeline.id
+																"
+																target="_blank"
 															>
-															edit
-														</v-list-item-title>
-													</v-list-item>
-													<v-list-item @click="remove(pipeline.id)">
-														<v-list-item-title>
-															<v-icon style="padding-right: 5px;"
-																>delete_sweep</v-icon
+																json
+															</a>
+														</v-list-item>
+														<v-list-item>
+															<a
+																:href="
+																	'/api/pipeline/data/csv/' +
+																		pipeline.id
+																"
+																target="_blank"
 															>
-															remove
-														</v-list-item-title>
-													</v-list-item>
-												</v-list>
-											</v-menu>
-										</td>
-									</tr>
-								</tbody>
-							</template>
-						</v-simple-table>
+																csv
+															</a>
+														</v-list-item>
+													</v-list>
+												</v-menu>
+											</td>
+											<td>
+												<v-menu offset-y>
+													<template v-slot:activator="{ on }">
+														<v-btn depressed text v-on="on">
+															<v-icon>more_vert</v-icon>
+														</v-btn>
+													</template>
+													<v-list>
+														<v-list-item @click="edit(pipeline.id)">
+															<v-list-item-title>
+																<v-icon style="padding-right: 5px;"
+																	>create</v-icon
+																>
+																edit
+															</v-list-item-title>
+														</v-list-item>
+														<v-list-item @click="remove(pipeline.id)">
+															<v-list-item-title>
+																<v-icon style="padding-right: 5px;"
+																	>delete_sweep</v-icon
+																>
+																remove
+															</v-list-item-title>
+														</v-list-item>
+													</v-list>
+												</v-menu>
+											</td>
+										</tr>
+									</tbody>
+								</template>
+							</v-simple-table>
+						</v-card>
 					</v-flex>
 				</v-layout>
 			</v-container>
