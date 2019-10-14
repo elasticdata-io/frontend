4652ffe0ecb8635d1bec5b957900e5f6fffb3956
sergey.tkachenko <serg.tkachenko@hotmail.com>
2019-10-14 20:47:36 +0300

    view ast data in table

--- src/views/PipelineDataView.vue
+++ src/views/PipelineDataView.vue
@@ -5,18 +5,30 @@
 				<v-col>
 					<v-btn :to="{ name: 'pipeline.edit', params: { id: id, state: 'edit' } }">
 						<v-icon class="mr-2">keyboard_backspace</v-icon>
-						назад до павука
+						назад до {{ pipeline.key }}
 					</v-btn>
 				</v-col>
 			</v-row>
 			<v-row>
 				<v-col>
 					<v-card>
+						<v-card-title>
+							{{ pipeline.key }}
+							<v-spacer></v-spacer>
+							<v-text-field
+								v-model="search"
+								append-icon="search"
+								label="Пошук"
+								single-line
+								hide-details
+							></v-text-field>
+						</v-card-title>
 						<v-card-text>
 							<v-data-table
 								:headers="headers"
 								:items="items"
 								:items-per-page="10"
+								:search="search"
 								class="elevation-1"
 							></v-data-table>
 						</v-card-text>
@@ -31,17 +43,22 @@
 </template>
 <script>
 import { mapGetters } from 'vuex';
-import { CURRENT_PIPELINE_LAST_DATA } from '../store/pipeline/getters';
-import { FETCH_PIPELINE_LAST_DATA } from '../store/pipeline/actions';
+import { CURRENT_PIPELINE, CURRENT_PIPELINE_LAST_DATA } from '../store/pipeline/getters';
+import { FETCH_PIPELINE, FETCH_PIPELINE_LAST_DATA } from '../store/pipeline/actions';
 
 export default {
 	components: {},
 	data() {
-		return {};
+		return {
+			search: null,
+		};
 	},
 	computed: {
 		...mapGetters('pipeline', {
 			lastData: CURRENT_PIPELINE_LAST_DATA,
+		}),
+		...mapGetters('pipeline', {
+			pipeline: CURRENT_PIPELINE,
 		}),
 		headers: function() {
 			const lastData = this.lastData;
@@ -73,6 +90,7 @@ export default {
 	methods: {},
 	created() {
 		this.$store.dispatch(`pipeline/${FETCH_PIPELINE_LAST_DATA}`, { id: this.id });
+		this.$store.dispatch(`pipeline/${FETCH_PIPELINE}`, { id: this.id });
 	},
 	props: {
 		id: {
