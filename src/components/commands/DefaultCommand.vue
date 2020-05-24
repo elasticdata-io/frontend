06bdcf4ea7a43637f1eef2a1473ff262d9ded4f6
sergey.tkachenko <serg.tkachenko@hotmail.com>
2020-05-24 03:45:33 +0300

    fix world

--- src/components/commands/DefaultCommand.vue
+++ src/components/commands/DefaultCommand.vue
@@ -5,8 +5,10 @@
 				<span class="number">{{ number }}</span>
 				<div class="name">{{ cmd }}</div>
 				<div class="prop-value" v-for="(value, prop) in commandParams" :key="prop + value">
-					<span class="prop">{{ prop }}</span>
-					<div v-if="!isObjectOrArray(value)" class="value">{{ value || '(empty)' }}</div>
+					<span class="prop" v-if="isNotEmpty(value)">{{ prop }}</span>
+					<div v-if="!isObjectOrArray(value) && isNotEmpty(value)" class="value">
+						{{ value }}
+					</div>
 					<div v-if="isObjectOrArray(value)" class="value">
 						<v-btn x-small depressed>JSON</v-btn>
 					</div>
@@ -60,6 +62,12 @@ export default {
 		},
 		isObjectOrArray(value) {
 			return typeof value === 'object';
+		},
+		isNotEmpty(value) {
+			if (value === undefined || value === null || value === '') {
+				return false;
+			}
+			return true;
 		},
 	},
 	props: {
