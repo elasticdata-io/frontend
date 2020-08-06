e96b4ef3b4bb8a63d07ac90d0d7137f40043c845
sergey tkachenko <serg.tkachenko@hotmail.com>
2020-08-06 14:48:12 +0300

    fixed display analytics commands

--- src/store/task-analyzed-commands/index.js
+++ src/store/task-analyzed-commands/index.js
@@ -1,22 +1,66 @@
 import Vue from 'vue';
+import * as _ from 'underscore';
 import * as action from './actions';
 import * as mutation from './mutations';
 import * as getter from './getters';
+
+/**
+ * @typedef {Object} LoopPageState
+ * @property {String} loopContext
+ * @property {Number} loopIndex
+ * @property {String} loopUuid
+ * @property {String} loopMaterializedUuid
+ */
 
 const state = {
 	showOnlyWithError: false,
 	loopIndexesByUuid: {},
 	loopIndexesByMaterializedUuidPath: {},
 	taskAnalyzedCommands: [],
+	/**
+	 * @type {Object.<string, LoopPageState>}
+	 */
+	loops: {},
 };
 
-const getChildMaterializedUuidPathList = (state, materializedUuidPath) => {
-	const keys = Object.keys(state.loopIndexesByMaterializedUuidPath).sort();
+const getChildMaterializedUuidPathArray = (state, materializedUuidPath) => {
+	const keys = Object.keys(state.loops).sort();
 	const findKeyIndex = keys.indexOf(materializedUuidPath);
 	if (findKeyIndex === -1) {
 		return [];
 	}
-	return keys.slice(findKeyIndex + 1, keys.length);
+	return keys
+		.slice(findKeyIndex + 1, keys.length)
+		.filter(x => x.startsWith(`${materializedUuidPath}_`));
+};
+
+const getParentMaterializedUuid = (state, materializedUuidPath) => {
+	const keys = Object.keys(state.loops)
+		.filter(x => {
+			return materializedUuidPath.startsWith(`${x}_`) || materializedUuidPath === x;
+		})
+		.sort();
+	const findKeyIndex = keys.indexOf(materializedUuidPath);
+	if (findKeyIndex === -1) {
+		return [];
+	}
+	return keys[findKeyIndex - 1];
+};
+
+const findLoopByMaterializedUuid = (state, materializedUuid) => {
+	const parentMaterializedUuid = getParentMaterializedUuid(state, materializedUuid);
+	if (!parentMaterializedUuid) {
+		return state.taskAnalyzedCommands.find(x => x.materializedUuidPath === materializedUuid);
+	}
+	const parentLoopPageState = state.loops[parentMaterializedUuid];
+	const parentContext = `${parentLoopPageState.loopContext}.${parentLoopPageState.loopIndex}`;
+	return state.taskAnalyzedCommands
+		.filter(x => x.cmd === 'loop')
+		.filter(x => x.materializedUuidPath === materializedUuid)
+		.find(x => {
+			const context = x.context ? `${x.dataContext}.${x.context}` : x.dataContext;
+			return context.startsWith(parentContext);
+		});
 };
 
 const mutations = {
@@ -25,43 +69,60 @@ const mutations = {
 	},
 
 	[mutation.SET_LOOP_DISPLAY_INDEX](state, { uuid, materializedUuidPath, index }) {
-		Vue.set(state.loopIndexesByUuid, uuid, index);
-		Vue.set(state.loopIndexesByMaterializedUuidPath, materializedUuidPath, index);
+		const loop = findLoopByMaterializedUuid(state, materializedUuidPath);
+		console.log(JSON.parse(JSON.stringify(loop)));
+		const loopContext = loop.designTimeConfig.context
+			? `${loop.dataContext}.${loop.designTimeConfig.context}`
+			: loop.dataContext;
+		const loopPageState = {
+			loopContext: loopContext,
+			loopIndex: index,
+			loopMaterializedUuid: materializedUuidPath,
+			loopUuid: uuid,
+		};
+		Vue.set(state.loops, materializedUuidPath, loopPageState);
 	},
 
 	[mutation.RESET_LOOP_DISPLAY_INDEX_FOR_CHILD](state, { materializedUuidPath }) {
-		const uuidPathList = getChildMaterializedUuidPathList(state, materializedUuidPath);
-		if (uuidPathList.length === 0) {
+		const childMaterializedUuidArray = getChildMaterializedUuidPathArray(
+			state,
+			materializedUuidPath
+		);
+		if (childMaterializedUuidArray.length === 0) {
 			return;
 		}
-		const uuidList = state.taskAnalyzedCommands
-			.filter(x => uuidPathList.indexOf(x.materializedUuidPath) !== -1)
-			.map(x => x.uuid);
-		const loopIndexesByUuid = state.loopIndexesByUuid;
-		uuidList.forEach(uuid => (loopIndexesByUuid[uuid] = 0));
-
-		const loopIndexesByMaterializedUuidPath = state.loopIndexesByMaterializedUuidPath;
-		uuidPathList.forEach(uuidPath => (loopIndexesByMaterializedUuidPath[uuidPath] = 0));
-
-		Vue.set(state, 'loopIndexesByMaterializedUuidPath', loopIndexesByMaterializedUuidPath);
-		Vue.set(state, 'loopIndexesByUuid', loopIndexesByUuid);
+		childMaterializedUuidArray.forEach(materializedUuid => {
+			const loops = state.loops;
+			const newLoop = findLoopByMaterializedUuid(state, materializedUuid);
+			const loopContext = newLoop.designTimeConfig.context
+				? `${newLoop.dataContext}.${newLoop.designTimeConfig.context}`
+				: newLoop.dataContext;
+			loops[materializedUuid] = {
+				loopContext: loopContext,
+				loopIndex: 0,
+				loopMaterializedUuid: newLoop.materializedUuidPath,
+				loopUuid: newLoop.uuid,
+			};
+			Vue.set(state, 'loops', loops);
+		});
 	},
 
 	[mutation.INITIALIZE_DEFAULT_LOOP_DISPLAY_INDEX](state, taskAnalyzedCommands) {
-		const loopIndexesByUuid = {};
-		const loopIndexesByMaterializedUuidPath = {};
-		for (const taskAnalyzedCommand of taskAnalyzedCommands) {
-			const cmd = taskAnalyzedCommand.cmd;
-			if (cmd !== 'loop') {
-				continue;
-			}
-			const uuid = taskAnalyzedCommand.uuid;
-			const materializedUuidPath = taskAnalyzedCommand.materializedUuidPath;
-			loopIndexesByUuid[uuid] = 0;
-			loopIndexesByMaterializedUuidPath[materializedUuidPath] = 0;
-		}
-		Vue.set(state, 'loopIndexesByUuid', loopIndexesByUuid);
-		Vue.set(state, 'loopIndexesByMaterializedUuidPath', loopIndexesByMaterializedUuidPath);
+		const loops = {};
+		let loopCommands = taskAnalyzedCommands.filter(x => x.cmd === 'loop');
+		loopCommands = _.uniq(loopCommands, x => x.materializedUuidPath);
+		loopCommands.forEach(loopCommand => {
+			const loopContext = loopCommand.designTimeConfig.context
+				? `${loopCommand.dataContext}.${loopCommand.designTimeConfig.context}`
+				: loopCommand.dataContext;
+			loops[loopCommand.materializedUuidPath] = {
+				loopUuid: loopCommand.uuid,
+				loopMaterializedUuid: loopCommand.materializedUuidPath,
+				loopIndex: 0,
+				loopContext: loopContext,
+			};
+		});
+		Vue.set(state, 'loops', loops);
 	},
 
 	[mutation.SET_TASK_ANALYZED_COMMANDS](state, taskAnalyzedCommands) {
@@ -70,9 +131,13 @@ const mutations = {
 };
 
 const actions = {
-	async [action.CHANGE_LOOP_DISPLAY_INDEX]({ commit }, { uuid, materializedUuidPath, index }) {
+	async [action.CHANGE_LOOP_DISPLAY_INDEX](
+		{ commit, state },
+		{ uuid, materializedUuidPath, index }
+	) {
 		commit(mutation.SET_LOOP_DISPLAY_INDEX, { uuid, materializedUuidPath, index });
 		commit(mutation.RESET_LOOP_DISPLAY_INDEX_FOR_CHILD, { materializedUuidPath });
+		console.log(JSON.parse(JSON.stringify(state.loops)));
 	},
 
 	async [action.INITIALIZE_TASK_ANALYZED_COMMANDS]({ commit }, taskAnalyzedCommands) {
@@ -87,17 +152,21 @@ const getters = {
 		if (state.showOnlyWithError) {
 			return taskAnalyzedCommands.filter(x => x.status === 'error');
 		}
-		let loopIndexesByMaterializedUuidPath = state.loopIndexesByMaterializedUuidPath;
-		const uuidPaths = Object.keys(loopIndexesByMaterializedUuidPath).sort();
-		const loopCommands = taskAnalyzedCommands.filter(x => x.cmd === 'loop');
-		for (const uuidPath of uuidPaths) {
-			const loopIndex = loopIndexesByMaterializedUuidPath[uuidPath];
-			const loopCommand = loopCommands.find(x => x.materializedUuidPath === uuidPath);
+		const uuids = Object.values(state.loops).map(x => x.loopUuid);
+		for (const uuid of uuids) {
+			const loopPageState = Object.values(state.loops).find(x => x.loopUuid === uuid);
+			const loopIndex = loopPageState.loopIndex;
+			const loopCommand = state.taskAnalyzedCommands.find(x => x.uuid === uuid);
+			if (!loopCommand) {
+				continue;
+			}
+			const uuidPath = loopCommand.materializedUuidPath;
 			const loopContext = loopCommand.designTimeConfig.context;
 			const loopDataContext = loopCommand.dataContext;
 			const loopFullContext = loopContext
 				? `${loopDataContext}.${loopContext}.${loopIndex}`
 				: `${loopDataContext}.${loopIndex}`;
+			console.log(`loopFullContext: ${loopFullContext}`);
 			taskAnalyzedCommands = taskAnalyzedCommands.filter(command => {
 				if (command.materializedUuidPath === uuidPath) {
 					// todo: display only loop command with need context
@@ -113,7 +182,12 @@ const getters = {
 		}
 		return taskAnalyzedCommands;
 	},
-	[getter.LOOP_DISPLAY_INDEXES_BY_UUID]: state => state.loopIndexesByUuid,
+	[getter.LOOP_DISPLAY_INDEXES_BY_UUID]: state => {
+		const loopIndexesByUuid = {};
+		Object.values(state.loops).forEach(x => (loopIndexesByUuid[x.loopUuid] = x.loopIndex));
+		return loopIndexesByUuid;
+	},
+	[getter.LOOPS]: state => state.loops,
 };
 
 export default {
