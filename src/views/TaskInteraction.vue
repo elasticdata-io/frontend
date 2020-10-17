3b8243a6240ac0f98231f5d6a255e3e66d612a93
sergey.tkachenko <serg.tkachenko@hotmail.com>
2020-10-17 19:51:35 +0300

    show interactive

--- src/views/TaskInteraction.vue
+++ src/views/TaskInteraction.vue
@@ -1,33 +1,19 @@
 <template>
 	<v-content>
-		<template>
-			<v-tabs v-model="tab">
+		<template v-if="userInteractions">
+			<v-tabs v-model="tab" @change="documentUnpack()">
 				<v-tab v-for="userInteraction in userInteractions" v-bind:key="userInteraction.id">
 					{{ userInteraction.currentUrl }}
 				</v-tab>
 			</v-tabs>
-			<v-tabs-items v-model="tab">
-				<v-tab-item
-					v-for="userInteraction in userInteractions"
-					v-bind:key="userInteraction.id"
-				>
-					<v-card flat>
-						<v-card-text>
-							<img
-								class="page-screenshot"
-								:src="userInteraction.jpegScreenshotLink"
-								:width="userInteraction.pageWidthPx"
-								:height="userInteraction.pageHeightPx"
-							/>
-						</v-card-text>
-					</v-card>
-				</v-tab-item>
-			</v-tabs-items>
+			<v-card flat>
+				<v-card-text id="window"></v-card-text>
+			</v-card>
 		</template>
 	</v-content>
 </template>
 <script lang="ts">
-import { documentPack } from 'web-page-teleport';
+import { documentUnpack } from 'web-page-teleport';
 import { FETCH_USER_INTERACTIONS } from '@/store/user-interaction/actions';
 import { mapGetters } from 'vuex';
 import { USER_INTERACTIONS } from '@/store/user-interaction/getters';
@@ -41,13 +27,32 @@ export default {
 			USER_INTERACTIONS: USER_INTERACTIONS,
 		}),
 		userInteractions(): UserInteraction[] {
-			return this.USER_INTERACTIONS;
+			return this.USER_INTERACTIONS || [];
+		},
+		currentUserInteraction: function() {
+			return this.userInteractions[this.tab];
 		},
 	},
+	watch: {},
 	data: () => {
 		return {
 			tab: null,
 		};
+	},
+	methods: {
+		documentUnpack() {
+			const toEl = document.getElementById('window');
+			if (toEl) {
+				const userInteraction = this.currentUserInteraction;
+				documentUnpack({
+					screenshotSrc: userInteraction.jpegScreenshotLink,
+					width: userInteraction.pageWidthPx,
+					height: userInteraction.pageHeightPx,
+					viewElements: userInteraction.pageElements,
+					toElement: toEl,
+				});
+			}
+		},
 	},
 	props: {
 		taskId: {
@@ -55,13 +60,10 @@ export default {
 			required: true,
 		},
 	},
-	async created() {
-		await this.$store.dispatch(`userInteraction/${FETCH_USER_INTERACTIONS}`, {
+	created() {
+		this.$store.dispatch(`userInteraction/${FETCH_USER_INTERACTIONS}`, {
 			taskId: this.taskId,
 		});
-	},
-	async mounted() {
-		console.log(await documentPack(document));
 	},
 };
 </script>
