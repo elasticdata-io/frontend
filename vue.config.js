7f95c9a22d4b34aeab9b4a17f354ab93d80a907e
s.tkachenko <serg.tkachenko@hotmail.com>
2021-10-17 18:20:39 +0300

    add workers page

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	devServer: {
 		host: 'localhost',
 		proxy: {
-			'^/cloud-manager/': {
+			'^/worker-manager/': {
 				target: 'http://localhost:3005',
 				changeOrigin: true,
 				autoRewrite: true,
